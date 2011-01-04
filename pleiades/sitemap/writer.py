194e7f15a9d14a0243547136126c28ace527e96a
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2011-01-04 21:16:26 +0000

    Convert to a zopectl script.

--- pleiades/sitemap/writer.py
+++ pleiades/sitemap/writer.py
@@ -1,18 +1,21 @@
+from optparse import OptionParser
+
 from Products.Five.browser import BrowserView
 from Products.CMFCore.utils import getToolByName
 
+from pleiades.dump import secure
 from pleiades.sitemap import SitemapIndex, SitemapWriter
 
 
-class PlacesSitemapDumper(BrowserView):
+class PlacesWriter(object):
     
-    def __call__(self):
+    def __call__(self, context, destination):
         index = SitemapIndex(
                     'http://pleiades.stoa.org', 
                     batchsize=10000, 
                     gzip=True
                     )
-        catalog = getToolByName(self.context, 'portal_catalog')
+        catalog = getToolByName(site, 'portal_catalog')
         results = catalog.searchResults(
                     portal_type='Place',
                     review_state='published',
@@ -21,6 +24,18 @@ class PlacesSitemapDumper(BrowserView):
                     )
         index.process(results)
         writer = SitemapWriter()
-        writer.write(index, '/tmp')
+        writer.write(index, destination)
         
-        return 'Complete'
+
+if '__name__' == '__main__':
+
+    parser = OptionParser()
+    parser.add_option(
+        "-d", "--destination", dest="destination",
+        help="Sitemap file(s) destination")
+    opts, args = parser.parse_args(sys.argv[1:])
+
+    site = app['plone']
+    secure(site, 'admin')
+    PlacesWriter()(site, opts.destination)
+
